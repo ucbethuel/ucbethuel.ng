26f35fddc7b254380df0b810e420eb1ef0d200bb
Uchenna Bethel Orji <bethuelorji@gmail.com>
2025-05-28 12:27:29 +0100

    Still on logo update, so that it works on vercel

--- frontend/src/components/Headers.jsx
+++ frontend/src/components/Headers.jsx
@@ -10,7 +10,7 @@ const Headers = () => {
             href="#"
           >
             <img
-              src="../src/assets/logo-2-trans.png"
+            src={new URL("/src/assets/logo-1-trans.png", import.meta.url).href}
               alt="UC Codes Logo"
               width="48"
               height="38"
